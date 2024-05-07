2125494393cd6cf6f50b36b9776da66d5a998120
dbugajny <dbugajny@qualtrics.com>
2024-05-07 09:47:20 +0200

    commit

--- notebooks/hyperparameters.py
+++ notebooks/hyperparameters.py
@@ -1,14 +1,14 @@
 BATCH_SIZE = 128
 STEPS_PER_EPOCH = 2106 // BATCH_SIZE
-IMAGE_SIZE = (52, 26)
-LATENT_DIM = 64
+IMAGE_SIZE = (64, 32)
+LATENT_DIM = 16
 
-RECONSTRUCTION_LOSS_WEIGHT = 100
+RECONSTRUCTION_LOSS_WEIGHT = 50
 KL_LOSS_WEIGHT = 1
 
 ENCODER_MODEL_HYPERPARAMETERS = {
     "image_shape": (64, 32, 3),
-    "latent_dim": 64,
+    "latent_dim": LATENT_DIM,
     "conv_block_filters": [32, 32, 64],
     "conv_block_kernel_sizes": [3, 3, 3],
     "conv_block_strides": [2, 2, 2],
